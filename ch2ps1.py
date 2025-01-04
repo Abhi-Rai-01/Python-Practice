41936fafb97024d7c53c70bbcc0164d9c22e4bcf
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-04 22:22:10 +0530

    Did problem 2

---
+++ ch2ps1.py
@@ -0,0 +1,5 @@
+a = int(input("Enter 1st number: "))
+
+b = int(input("Enter 2nd number: "))
+
+print("The sum of 1st number and 2nd number is: ", a + b)
---
+++ ch2ps2.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number :"))
+b = int(input("Enter another number which is to be Divisor :"))
+c = int(a % b)
+print("The remainder is :", c)
