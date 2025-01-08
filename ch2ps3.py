64d4200c9a702b216247bc0a2617eb08995735ae
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-08 19:36:06 +0530

    Some problem solved

---
+++ ch2ps3.py
@@ -0,0 +1,21 @@
+a = input("Enter any value you like :")
+b = type(a)
+c = 12
+d = type(c)
+e = 5.34
+f = type(e)
+g = "Abhi"
+h = type(g)
+i = 5>3
+j = type(i)
+k = None
+l = type(k)
+
+
+
+print(b)
+print(d)
+print(f)
+print(h)
+print(j)
+print(k)
---
+++ ch2ps5.py
@@ -0,0 +1,5 @@
+a = int(input("Enter 1st number :"))
+b = int(input("Enter 2nd number :"))
+c = (a + b)
+d = c/2
+print("The average of two numbers is :",d)
